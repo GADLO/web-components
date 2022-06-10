5331f8dbf3ba36272a6791b846a537f607ab9b33
GADLO <geekarea@outlook.com>
2022-06-10 16:16:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//Autonomous custom elements （自主自定义标签）
+class MyElement extends HTMLElement {
+  cobstructor() {
+    super();
+  }
+
+  connectedCallback() {}
+
+  disconnectedCallback() {}
+
+  static get observeAttributes() {
+    return [];
+  }
+
+  attributeChangeCallback() {}
+
+  adoptedCallback() {}
+}
